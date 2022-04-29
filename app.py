f557a5c47320957796b31dd2925b2828f7d0d4bc
renatagarib <50972252+renatagarib@users.noreply.github.com>
2022-04-29 13:22:26 -0300

    read_all + db

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, Response, request
+import pymysql
+import json
+
+app = Flask(__name__)
+
+def to_json(list):
+    return {'CPF': list[0], 
+            'nome': list[1], 
+            'idade': list[2], 
+            'email': list[3], 
+            'media': list[4], 
+            'historico': list[5], 
+            'id_turma': list[6]}
+
+@app.route("/students", methods=["GET"])
+def read_all():
+    connection = pymysql.connect(host='localhost',
+                                user='root',
+                                password='nilmar21',
+                                database='mydb')
+
+    with connection:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM `aluno`"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            result_jason = [to_json(student) for student in result]
+            print(result_jason)
+            return create_response(200, 'students', result_jason)
+
+
+
+
+def create_response(status, content_name, content, message=False):
+    body = {}
+    body[content_name] = content
+
+    if (message):
+        body['message'] = message
+
+    return Response(json.dumps(body), status=status, mimetype="application/json")
